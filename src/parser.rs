61b177fcaaa31ae4b41663690fd1f2c28199a8c6
ykyki <bsyki@pm.me>
2023-06-21 08:46:24 +0900

    create fn to remove comments and spaces

--- src/parser.rs
+++ src/parser.rs
@@ -4,11 +4,58 @@ use std::collections::BTreeMap;
 
 use crate::ParseError;
 
-pub(super) fn parse_paragraphs(_input: &str) -> Result<ResultMap, ParseError> {
+pub(super) fn parse_paragraphs(input: &str) -> Result<ResultMap, ParseError> {
+    let _ = corrent_lines(input.to_string());
     todo!();
 }
 
 #[derive(Debug)]
 pub(super) struct ResultMap {
     value: BTreeMap<Key, Node>, // todo
+}
+
+fn corrent_lines(input: String) -> String {
+    const COMMENT_DISC: &str = "%";
+
+    input
+        .lines()
+        .map(|x| x.trim_start())
+        .map(
+            |x| x.split(COMMENT_DISC).next().unwrap(), // 行末コメント除去
+        )
+        .map(|x| x.trim_end())
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[allow(non_snake_case)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correct_lines() {
+        macro_rules! assert_correct_lines {
+            ($input:expr, $expected:expr) => {
+                assert_eq!(corrent_lines($input.to_string()), $expected.to_string());
+            };
+        }
+
+        assert_correct_lines!("", "");
+        assert_correct_lines!("%", "");
+        assert_correct_lines!(" a b c ", "a b c");
+
+        assert_correct_lines!(
+            r"abc
+            
+            def",
+            "abc\n\ndef"
+        );
+
+        assert_correct_lines!(
+            r"数式$x$や % コメント
+            \(y\)など %",
+            "数式$x$や\n\\(y\\)など"
+        );
+    }
 }
