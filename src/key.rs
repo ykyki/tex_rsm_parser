ba766be6f85b51049f48abb47f4a8fda68a5ed01
ykyki <bsyki@pm.me>
2023-06-21 00:35:48 +0900

    create KeyCounter

--- src/key.rs
+++ src/key.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub(super) struct Key {
     value: u32,
 }
@@ -6,5 +6,40 @@ pub(super) struct Key {
 impl Key {
     fn new() -> Self {
         Self { value: 0 }
+    }
+
+    fn increment(&mut self) {
+        self.value += 1;
+    }
+}
+
+#[derive(Debug)]
+pub(super) struct KeyCounter {
+    next: Key,
+}
+
+impl KeyCounter {
+    pub fn new() -> Self {
+        Self { next: Key::new() }
+    }
+
+    pub fn count(&mut self) -> Key {
+        let k = self.next.clone();
+        self.next.increment();
+        k
+    }
+}
+
+#[allow(non_snake_case)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_KeyCounter() {
+        let mut kc = KeyCounter::new();
+        assert_eq!(kc.count(), Key { value: 0 });
+        assert_eq!(kc.count(), Key { value: 1 });
+        assert_eq!(kc.count(), Key { value: 2 });
     }
 }
